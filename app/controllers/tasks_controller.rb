7436e0b1d37cea2de25479f49ff1d31f99c1eb70
tmk616window <tmk616window@gmail.com>
2020-04-13 18:27:28 +0000

    finish tasks_index flont

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,26 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all.order(id: :desc)
+    gon.data = []
+    6.times do
+      gon.data << rand(100.0)
+    end
+    
   end
-
+  
   def new
   end
-
+  
   def show
   end
-
+  
   def edit
+  end
+  
+  private
+  
+  
+  def task_params
+    params.require(:task).permit(:job)
   end
 end
