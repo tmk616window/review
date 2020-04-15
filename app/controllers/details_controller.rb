f46f07c3f51ff8893bea224580290fd48926006e
tmk616window <tmk616window@gmail.com>
2020-04-15 21:10:36 +0000

    finish tasks_show

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -4,14 +4,13 @@ class DetailsController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @users = User.all
     @follow = Follow.new
     @follows = Follow.where(user_id: params[:id])
     @follow_user = @follows.find_by(follower_id: current_user.id)
     @follow_current_user = Follow.where(follower_id: @user.id)
-    
   end
-
+  
   def edit
   end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,13 @@ class UsersController < ApplicationController
   def edit
   end
   
+  def create
+    @user = User.find(params[:id])
+    @uder.save
+    redirect_to '/tasks'
+  end
   
+  def user_params
+    params.require(:user).permit(:user_id,:coment,:old,:programing_language,:job_past)
+  end
 end
